ed2fb9a6574abfd27f607861bf74f12f0ed69bea
Muhammad Hamza Sohail <67511980+Hamzenium@users.noreply.github.com>
2022-12-02 01:20:18 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,14 +1,6 @@
-document.querySelector("add").onclick = function summation(){  
-    var data_1 = document.getElementById("folder").value
-    console.log(data_1)
-    var data_2 = document.getElementById("lfooter").value  
-    console.log(data_2)
-    var data_3 = document.getElementById("mfooter").value 
-    console.log(data_3)
-    var data_4 = document.getElementById("rfooter").value 
-    console.log(data_4)
-    data = eel.path_adder(data_1)
-    eel.path_adder(data,data_2,data_3,data_4,folder)
-}  
-
-console.log("erer")
+document.getElementById('bt').addEventListener('click', async()=>{
+    await eel.python_connector(document.getElementById('folder').value,
+    document.getElementById('lfooter').value,
+    document.getElementById('mfooter').value,
+    document.getElementById('rfooter').value)
+})
